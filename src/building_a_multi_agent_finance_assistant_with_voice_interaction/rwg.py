4cde379b91934ad9cfe0a0d39313e0a44b8e516e
Sannidhay Jangam <76656957+sannidhayj20@users.noreply.github.com>
2025-05-28 17:09:36 +0530

    Delete src/building_a_multi_agent_finance_assistant_with_voice_interaction/rwg.py

--- src/building_a_multi_agent_finance_assistant_with_voice_interaction/rwg.py
+++
@@ -1,6 +0,0 @@
-api_key = "AIzaSyAz2ebIcVe9EN_SU-CB-n3wTstCqg-R4t8"
-import google.generativeai as genai
-genai.configure(api_key=api_key)
-model = genai.GenerativeModel('gemini-2.0-flash')
-response = model.generate_content("How does AI work?")
-print(response.text)
